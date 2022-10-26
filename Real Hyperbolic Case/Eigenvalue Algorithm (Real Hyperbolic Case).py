48e7fae72d2c4a8ca010184cd22ae6c8c5589d01
jacobtlinden <jacob.linden@gmail.com>
2022-10-25 19:39:23 -0700

    update algorithm

--- Real Hyperbolic Case/Eigenvalue Algorithm (Real Hyperbolic Case).py
+++ Real Hyperbolic Case/Eigenvalue Algorithm (Real Hyperbolic Case).py
@@ -9,23 +9,20 @@ import random
 
 # define disjoint disks (Schottky group) and associated functions
 
-N = 6 # number of disks
+N = 2 # number of disks
 
 # define disks in terms of centers and radii
-centers = np.array([-1, 1, -4, 4, -7, 7]).astype(complex)
-radii = np.array([1, 1, 1, 1, 1, 1]).astype(complex)
+centers = np.array([-2, 2]).astype(complex)
+radii = np.array([1, 1]).astype(complex)
 
-
-# create list of indices of paired disks
-pairs = np.array([[0,1], [1,0], [2,3], [3,2], [4,5], [5,4]])
 
 # create list of disk/domain indices corresponding to sample points x_i (i.e. which original disk contains x_i?)
-disks = np.array([0, 1, 2, 3, 4, 5])
+disks = np.array([0, 1])
 
 # generate sample points in each disk
 x = np.empty(N).astype(complex)
 for i in range(N):
-    theta = random.uniform(0, 2*np.pi)
+    theta = random.uniform(0, np.pi) # we want upper half of disk
     radius = random.uniform(0, radii[i])
     x[i] = centers[i]+radius*(np.cos(theta)+1j*np.sin(theta))
 
@@ -35,10 +32,11 @@ for i in range(N):
 
 
 # define Mobius transformation, in terms of centers and radii
-# a and r are center and radius of first disk, b and s center and radius of second
+# a and r are center and radius of the disk about which the reflection is taken
+# this map is its own inverse
 
-def mob(a,r, b,s, z):
-    val = b - s*r/(z-a)
+def mob(a, r, z):
+    val = a - r**2/(z-a)
         
     return val
     
@@ -50,8 +48,8 @@ def mob(a,r, b,s, z):
 
 # define derivative of Mobius transformation analagously
 
-def mob_prime(a,r, b,s, z):
-    val = s*r/(z-a)**2
+def mob_prime(a, r, z):
+    val = r**2/(z-a)**2
         
     return val
     
@@ -68,10 +66,10 @@ y = np.zeros((N,N)).astype(complex)
 
 for i in range(N):
     for j in range(N):
-        # we want inverse mobius transform, which we can find just by reversing order of parameters
-        if not ( [disks[i],disks[j]] in pairs.tolist() ):
-            ind = pairs[i]
-            y[i,j] = mob(centers[ind[1]], radii[ind[1]], centers[ind[0]], radii[ind[0]], x[j])
+        # we want inverse mobius transform, which is the same as the transform itself
+        if i != j:
+            ind = disks[i]
+            y[i,j] = mob(centers[ind], radii[ind], x[j])
 
 #print(y)
 
@@ -85,9 +83,9 @@ T = np.zeros((N,N))
 
 for i in range(N):
     for j in range(N):
-        if not ( [disks[i],disks[j]] in pairs.tolist() ):
-            ind = pairs[i]
-            T[i,j] = 1/abs(mob_prime(centers[ind[0]], radii[ind[0]], centers[ind[1]], radii[ind[1]],                                        y[i,j]))
+        if i != j:
+            ind = disks[i]
+            T[i,j] = 1/abs(mob_prime(centers[ind], radii[ind], y[i,j]))
     
 #print(T)
 
@@ -145,19 +143,16 @@ alpha_P = alpha_mid
 
 new_x = np.empty(0).astype(complex)
 new_disks = np.empty(0)
-new_pairs = np.empty((0,2))
 
 for i in range(N):
     for j in range(N):
         if y[i,j] != 0:
             new_x = np.append(new_x, y[i,j])
             new_disks = np.append(new_disks, disks[i])
-            new_pairs = np.append(new_pairs, pairs[i].reshape(1,2), axis=0)
 
 
 x = new_x
 disks = new_disks.astype(int)
-pairs = new_pairs.astype(int)
 N = x.size
 
 # Notes:
@@ -167,6 +162,8 @@ N = x.size
 # doms exists to check i -> j
 # the problem with this is that the blocks in the refinement are no longer the whole first disk of the mapping,
 # so not guaranteed that anything outside of the second disk will have positive interssection with image
+
+# this note ^^^ is from the pre 4-15 version, the condition is now "if i != j", and this has the same issue
 
 print(x)
 
